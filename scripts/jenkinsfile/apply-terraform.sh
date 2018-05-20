c90e06eec2b34df120914af1dca16420af154a69
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-20 14:24:30 +1000

    Removing Jenkins variable escape

--- scripts/jenkinsfile/apply-terraform.sh
+++ scripts/jenkinsfile/apply-terraform.sh
@@ -6,4 +6,4 @@ mkdir Software && \
 wget -P Software https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip && \
 unzip -d Software Software/terraform_0.11.7_linux_amd64.zip && rm -rf Software/*.zip
 
-export terraform=\$PROJECT_ROOT/Software/terraform
+terraform=$PROJECT_ROOT/Software/terraform
