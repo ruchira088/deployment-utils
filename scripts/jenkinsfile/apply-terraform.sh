04c6783f159af629b9beeccce5bf2440ebe679b8
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-20 16:58:39 +1000

    Refactoring

--- scripts/jenkinsfile/apply-terraform.sh
+++ scripts/jenkinsfile/apply-terraform.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-apt-get update && apt-get install wget unzip -y
+export PROJECT_ROOT=`pwd`
+export terraform=$PROJECT_ROOT/Software/terraform
 
-mkdir Software && \
-wget -P Software https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip && \
-unzip -d Software Software/terraform_0.11.7_linux_amd64.zip && rm -rf Software/*.zip
+beforeApply() {
+    apt-get update && apt-get install jq wget unzip -y
 
-terraform=$PROJECT_ROOT/Software/terraform
+    mkdir Software && \
+    wget -P Software https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip && \
+    unzip -d Software Software/terraform_0.11.7_linux_amd64.zip && rm -rf Software/*.zip
+
+    cd dev-ops/terraform
+    sed -i "s/BACKEND_KEY/`echo $JOB_NAME | tr / -`/g" resources.tf
+
+    $terraform init
+}
+
+afterApply() {
+    $terraform show
+
+    DOCKER_REPOSITORY_URL=`$terraform output -json | jq .dockerRepositoryUrl.value`
+
+    echo $DOCKER_REPOSITORY_URL >> docker-repository-url.txt
+
+    cd $PROJECT_ROOT
+}
