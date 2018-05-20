509e2b5bb3dc4021f051f1d7ca0aa10347f14be3
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-20 14:34:32 +1000

    Added push-docker-image.sh

--- scripts/jenkinsfile/apply-terraform.sh
+++ scripts/jenkinsfile/apply-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install wget unzip jq -y
+apt-get update && apt-get install wget unzip -y
 
 mkdir Software && \
 wget -P Software https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip && \
---
+++ scripts/jenkinsfile/push-docker-image.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+apk -v --update add python py-pip git && \
+pip install awscli --upgrade --user && \
+ln -sf $HOME/.local/bin/aws /usr/local/bin
+
+aws ecr get-login --no-include-email --region ap-southeast-2 | sh
+
+DOCKER_REPOSITORY_URL=`cat dev-ops/terraform/docker-repository-url.txt`
+GIT_COMMIT=`git rev-parse HEAD | cut -c1-8`
+
+echo "DOCKER_REPOSITORY_URL = $DOCKER_REPOSITORY_URL"
+
+DOCKER_IMAGE_TAG=$JOB_NAME-$BUILD_NUMBER
+docker build -t $DOCKER_IMAGE_TAG -f dev-ops/Dockerfile .
+
+docker tag $DOCKER_IMAGE_TAG:latest `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:build-number-$BUILD_NUMBER
+docker push `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:build-number-$BUILD_NUMBER
+
+docker tag $DOCKER_IMAGE_TAG:latest `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:\$GIT_COMMIT
+docker push `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:$GIT_COMMIT
+
+docker tag $DOCKER_IMAGE_TAG:latest `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:latest
+docker push `echo $DOCKER_REPOSITORY_URL | tr -d '"'`:latest
+
+docker images
