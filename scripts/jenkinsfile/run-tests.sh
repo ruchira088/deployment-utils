d69a28b7e8f84100e594dfafe4f878d141387733
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-20 16:44:46 +1000

    Executing the test suite in Ubuntu

--- scripts/jenkinsfile/run-tests.sh
+++ scripts/jenkinsfile/run-tests.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-apk -v --update add python py-pip git && \
-pip install awscli --upgrade --user && \
-ln -sf $HOME/.local/bin/aws /usr/local/bin
+apt-get update && \
+apt-get install \
+    apt-transport-https \
+    bc \
+    ca-certificates \
+    software-properties-common \
+    python-pip \
+    python-dev \
+    build-essential \
+    -y
 
-DOCKER_IMAGE_TAG=$JOB_NAME-$BUILD_NUMBER
-docker build -t $DOCKER_IMAGE_TAG -f dev-ops/Dockerfile .
-docker run $DOCKER_IMAGE_TAG test
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list && \
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 && \
+apt-get update && \
+apt-get install sbt -y
+
+pip install awscli --upgrade --user && \
+sudo ln -sf $HOME/.local/bin/aws /usr/local/bin
+
+sbt testWithCoverage
