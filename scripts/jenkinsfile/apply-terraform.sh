801713cb1aa3b94e6dc4d14b9baee9ce173abb08
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-20 13:58:50 +1000

    Exporting terraform in the script

--- scripts/jenkinsfile/apply-terraform.sh
+++ scripts/jenkinsfile/apply-terraform.sh
@@ -6,4 +6,4 @@ mkdir Software && \
 wget -P Software https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip && \
 unzip -d Software Software/terraform_0.11.7_linux_amd64.zip && rm -rf Software/*.zip
 
-terraform=\$PROJECT_ROOT/Software/terraform
+export terraform=\$PROJECT_ROOT/Software/terraform
