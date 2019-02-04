e936b964b6a90860448aa9043be13750e34e5808
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-02-04 21:30:08 +1100

    Grouping k8s scripts

--- scripts/micro-k8s/create.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-snap install microk8s --classic && \
-microk8s.enable dns dashboard ingress
-
-# https://github.com/ubuntu/microk8s/issues/75
-# Fix public DNS resolution inside pods
-sudo iptables -P FORWARD ACCEPT
