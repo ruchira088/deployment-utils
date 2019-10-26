51b3cf43c7f29c28d3d55a64531f5815e8a69be4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-10-26 18:20:54 +1100

    Updating the K8s GKE script

--- scripts/k8s/gke/gke.sh
+++ scripts/k8s/gke/gke.sh
@@ -5,17 +5,20 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
 
+# Waiting for tiller to be deployed
+kubectl rollout status deployment tiller-deploy -n kube-system
+
 # Deploy nginx-ingress
 helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true
 
 # Install the CustomResourceDefinition resources separately
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
 
 # Create the namespace for cert-manager
 kubectl create namespace cert-manager
 
-# Label the cert-manager namespace to disable resource validation
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
 
 # Update your local Helm chart repository cache
 helm repo update
@@ -24,9 +27,13 @@ helm repo update
 helm install \
   --name cert-manager \
   --namespace cert-manager \
-  --version v0.6.0 \
-  stable/cert-manager
+  --version v0.11.0 \
+  jetstack/cert-manager
 
 aws s3 cp s3://cert-manager.ruchij.com/cert-manager-secrets.yaml . ; kubectl apply -f cert-manager-secrets.yaml; rm cert-manager-secrets.yaml
+
+echo "Sleeping for 2 minutes..."
+sleep 120
+echo "Sleeping completed"
 
 aws s3 cp s3://cert-manager.ruchij.com/cluster-issuer.yaml . ; kubectl apply -f cluster-issuer.yaml; rm cluster-issuer.yaml
