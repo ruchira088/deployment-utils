a018e9a2d31e81277f5abbc86249a85acf908db7
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-11-02 15:59:44 +1100

    Updating cert-manager scripts

--- scripts/k8s/cert-manager/rbac-disabled.sh
+++ scripts/k8s/cert-manager/rbac-disabled.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-helm init && \
-helm install --name cert-manager --namespace kube-system --set rbac.create=false stable/cert-manager
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+
+helm init
+
+kubectl create namespace cert-manager
+
+helm repo add jetstack https://charts.jetstack.io
+
+helm repo update
+
+helm install \
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.11.0 \
+  jetstack/cert-manager
